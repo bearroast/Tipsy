01a7c180bf2c21304df3043f8ffb99abc29b1b4d
Bjørn Rostad <hi@bjornrostad.no>
2014-08-17 16:05:30 -0700

    segmented controller working

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
     
     
     
@@ -31,8 +32,11 @@ class ViewController: UIViewController {
     @IBAction func onEditingChanged(sender: AnyObject) {
         println("User editing bill")
         
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
         var billAmount = NSString(string: billField.text).doubleValue
-        var tip = billAmount * 0.2
+        var tip = billAmount * tipPercentage
         var total = billAmount + tip
         
         tipLabel.text = "$\(tip)"
@@ -43,5 +47,8 @@ class ViewController: UIViewController {
         
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
 
