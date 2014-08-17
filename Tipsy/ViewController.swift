e30d5834fee5e2e0c047d537d2bf11e0a13d7131
Bjørn Rostad <hi@bjornrostad.no>
2014-08-17 15:27:14 -0700

    basic ui

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
     }
 
     override func didReceiveMemoryWarning() {
