45f96d93b8e2ba7a55f73ca42caea034ba4f9bd3
Bjørn Rostad <hi@bjornrostad.no>
2014-08-17 18:36:03 -0700

    ux improvements

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -12,10 +12,15 @@ class ViewController: UIViewController {
                             
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var tipLabelForTwo: UILabel!
+    @IBOutlet weak var tipLabelForThree: UILabel!
+    @IBOutlet weak var tipLabelForFour: UILabel!
+    
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var totalLabelForTwo: UILabel!
     @IBOutlet weak var totalLabelForThree: UILabel!
     @IBOutlet weak var totalLabelForFour: UILabel!
+    
     @IBOutlet weak var tipControl: UISegmentedControl!
     
     
@@ -23,41 +28,61 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tipLabel.text = "$0.00"
-        totalLabel.text = "-"
-        totalLabelForTwo.text = "-"
-        totalLabelForThree.text = "-"
-        totalLabelForFour.text = "-"
-    
+        var tipPlaceholder = ""
+        var totalPlaceholder = "-"
+        
+        tipLabel.text = tipPlaceholder
+        totalLabel.text = totalPlaceholder
+
+        tipLabelForTwo.text = tipPlaceholder
+        totalLabelForTwo.text = totalPlaceholder
+
+        tipLabelForThree.text = tipPlaceholder
+        totalLabelForThree.text = totalPlaceholder
+
+        tipLabelForFour.text = tipPlaceholder
+        totalLabelForFour.text = totalPlaceholder
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
     @IBAction func onEditingChanged(sender: AnyObject) {
         println("User editing bill")
         
-        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentages = [0.14, 0.18, 0.22]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
         var billAmount = NSString(string: billField.text).doubleValue
+        
         var tip = billAmount * tipPercentage
+        var tipForTwo = (billAmount * tipPercentage) / 2
+        var tipForThree = (billAmount * tipPercentage) / 3
+        var tipForFour = (billAmount * tipPercentage) / 4
+        
         var total = billAmount + tip
-        var totalForTwo = (billAmount + tip) / 2
-        var totalForThree = (billAmount + tip) / 3
-        var totalForFour = (billAmount + tip) / 4
+        var totalForTwo = (billAmount/2) + tipForTwo
+        var totalForThree = (billAmount/3) + tipForThree
+        var totalForFour = (billAmount/4) + tipForFour
         
-        tipLabel.text = "$\(tip)"
-        totalLabel.text = "$\(total)"
-        totalLabelForTwo.text = "$\(total)"
+
         
-        tipLabel.text = String(format: "$%.2f", tip)
+        tipLabel.text = String(format: "incl $%.2f tip", tip)
         totalLabel.text = String(format: "$%.2f", total)
+        
+        tipLabelForTwo.text = String(format: "incl $%.2f tip", tipForTwo)
         totalLabelForTwo.text = String(format: "$%.2f", totalForTwo)
+        
+        tipLabelForThree.text = String(format: "incl $%.2f tip", tipForThree)
         totalLabelForThree.text = String(format: "$%.2f", totalForThree)
+        
+        tipLabelForFour.text = String(format: "incl $%.2f tip", tipForFour)
         totalLabelForFour.text = String(format: "$%.2f", totalForFour)
+
         
     }
 
